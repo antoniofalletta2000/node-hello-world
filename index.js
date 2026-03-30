524dde616ca96f7a3ea5a7a5c16840825f6d9d26
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-30 12:26:05 +0200

    add watch script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { createServer } from 'node:http';
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
+  res.end('Hello Boolean!\n');
 });
 // starts a simple http server locally on port 3000
 server.listen(3000, '127.0.0.1', () => {
